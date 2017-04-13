3759c63ccc154f915363c3f05bf55d871fc8415c
Nikita Zhiltsov <nisovru@gmail.com>
2017-04-13 14:49:00 +0400

    Delete create.m

--- lab2/create.m
+++
@@ -1,6 +0,0 @@
-%create function, builds test matrix; args = (n = dim, alpha = paramA , beta = paramB)
-function res = create(n, alpha, beta)
-
-res = eye(n)*alpha + diag(beta*ones(1,n - 1), 1) + diag(beta*ones(1,n - 1), -1);
-res(1,1) = alpha - beta;
-end
